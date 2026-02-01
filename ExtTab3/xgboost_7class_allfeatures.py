24c06b3ecff5e52cfa058f7efaaf298e26e8a0b4
mkazukikom <163707889+mkazukikom@users.noreply.github.com>
2026-02-01 21:44:16 +0900

    Update output CSV filename for feature importance

--- ExtTab3/xgboost_7class_allfeatures.py
+++ ExtTab3/xgboost_7class_allfeatures.py
@@ -170,7 +170,7 @@ tmp_imp = pd.concat(imp_list, axis=1)
 tmp_imp_stat = pd.concat([tmp_imp.T.mean(), tmp_imp.T.std()],axis=1)
 tmp_imp_stat.columns=["mean","std"]
 tmp_imp_stat.sort_values(by="mean", ascending=False,inplace=True)
-tmp_imp_stat.to_csv("xgboost_2class_feature_importance.csv", index=True)
+tmp_imp_stat.to_csv("xgboost_7class_feature_importance.csv", index=True)
 print(tmp_imp_stat.iloc()[:30,:].to_markdown())
 
 imp_dict = {}
